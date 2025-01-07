3adcddaeed0753493c78bda7211e8ff347176f31
Pierre Guillot <guillotpierre6@gmail.com>
2025-01-07 09:51:35 +0100

    GuiApp: example of crash using the Arial font on Linux

--- examples/CMake/GuiApp/MainComponent.cpp
+++ examples/CMake/GuiApp/MainComponent.cpp
@@ -12,7 +12,7 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::FontOptions (16.0f));
+    g.setFont (juce::FontOptions ("Arial", 16.0f, 0));
     g.setColour (juce::Colours::white);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
