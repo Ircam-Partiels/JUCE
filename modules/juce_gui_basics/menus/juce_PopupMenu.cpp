3d8491537338512fa0304266421c22d08d08facb
Pierre Guillot <guillotpierre6@gmail.com>
2025-06-12 10:41:09 +0200

    Fix the extra trailing comma of the shortcut key description in juce_PopupMenu.cpp

--- modules/juce_gui_basics/menus/juce_PopupMenu.cpp
+++ modules/juce_gui_basics/menus/juce_PopupMenu.cpp
@@ -317,7 +317,7 @@ private:
             {
                 auto key = keypress.getTextDescriptionWithIcons();
 
-                if (shortcutKey.isNotEmpty())
+                if (key.isNotEmpty() && shortcutKey.isNotEmpty())
                     shortcutKey << ", ";
 
                 if (key.length() == 1 && key[0] < 128)
